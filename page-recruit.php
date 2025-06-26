c032e9692be797914bb322adf028a72f17d88cd7
keito noguchi <noguchi@hug-inc.co.jp>
2025-06-26 12:41:52 +0900

    6/17仮テキストや画像追加修正＆本番化 6/25各ボタンのテキスト修正＆追加 ・医療スタッフ求人はこちら→管理職・広報・医療スタッフ求人はこちら ・※飲食店求人は直接ご連絡ください→※ こちらの採用ページをしっかり見ていただき、このページから直接応募していただいた方が優先採用になります

--- page-recruit.php
+++ page-recruit.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
 <div id="develop">
   <div class="heading_ttl">
     <div class="heading_ttl_inn">
@@ -16,7 +15,6 @@
       </div>
     </div>
   </div>
-
   <section id="features">
     <div class="features_inn">
       <ul class="features_list">
@@ -34,9 +32,9 @@
         </li>
         <li>
           <div class="features_list_img">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit/sec1_03.svg" alt="アイコン全店舗駅近">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit/sec1_03.svg" alt="アイコン全院・全店舗駅近">
           </div>
-          <p>全店舗駅近</p>
+          <p>全院・全店舗駅近</p>
         </li>
       </ul>
       <div class="features_img">
@@ -44,30 +42,27 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit/features.jpg" alt="スタッフ集合写真" class="sp">
       </div>
     </div>
-  </section>
-
-  <section id="motto">
-    <h2 class="fadeIn"><span>社訓</span></h2>
-    <div class="motto_box fadeIn">
-      <p class="motto_txt">この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</p>
-      <div class="motto_img">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit/motto.png" alt="社訓" class="pc">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit/motto_sp.png" alt="社訓" class="sp">
-      </div>
+    <div class="features_btn">
+      <a href="https://www.futabaclinic.jp/recruit/03/" class="btn staff_btn fadeIn" target="_blank">
+        <p>管理職・広報・医療スタッフ<br>求人はこちら</p>
+      </a>
+      <a href="https://www.futabaclinic.jp/recruit/02/" class="btn ishi_btn fadeIn" target="_blank">
+        <p>医師求人はこちら</p>
+      </a>
+      <p class="features_notes fadeIn">※こちらの採用ページをしっかり見ていただき、<br>このページから直接応募していただいた方が<br class="sp">優先採用になります</p>
     </div>
   </section>
-
   <section id="job">
     <div class="inn job_inn">
       <h2 class="fadeIn"><span>募集職種</span></h2>
       <ul class="job_list fadeIn">
         <li>
-          <a href="#doctor">
+          <a href="https://www.futabaclinic.jp/recruit/02/" target="_blank">
             <p>MS事業部 (メディカルサービス)<span>医師</span></p>
           </a>
         </li>
         <li>
-          <a href="#office">
+          <a href="https://www.futabaclinic.jp/recruit/03/" target="_blank">
             <p>MS事業部 (メディカルサービス)<span>スタッフ</span></p>
           </a>
         </li>
@@ -84,180 +79,6 @@
       </ul>
     </div>
   </section>
-
-  <section id="doctor" class="fadeIn">
-    <div class="inn">
-      <h3 class="fadeIn">医師</h3>
-      <table class="fadeIn">
-        <tbody>
-          <tr>
-            <th>給与例【常勤】</th>
-            <td>
-              ●埼玉院・佐倉院・八千代院<br>
-              週5日	2,500万円～2,800万円<br>
-              週4日	2,100万円～2,400万円<br>
-              週3日	1,300万円～1,700万円
-              <br><br>
-              ●船橋院・本八幡院<br>
-              週5日	2,400万円～2,700万円<br>
-              週4日	2,000万円～2,300万円<br>
-              週3日	1,200万円～1,600万円
-              <br><br>
-              ●北千住院・新小岩院・錦糸町院<br>
-              週5日	2,300万円～2,600万円<br>
-              週4日	1,900万円～2,200万円<br>
-              週3日	1,200万円～1,600万円
-            </td>
-          </tr>
-          <tr>
-            <th>給与例【非常勤】</th>
-            <td>
-              ●日勤<br>
-              9:00～17:00（休憩1時間）<br>
-              9万円～12万円
-              <br><br>
-              ●夜間オンコール（全日）<br>
-              18:00～翌朝8:30（勤務時間は応相談）<br>
-              自宅待機　2万円+出動手当2万円/往診1回につき<br>
-              クリニック待機　4万円+出動手当1万円/往診1回につき
-              <br><br>
-              ●日中オンコール（土日祝）<br>
-              9:00～18:00<br>
-              自宅待機　2万円+出動手当2万円/往診1回につき<br>
-              クリニック待機　4万円+出動手当1万円/往診1回につき
-              <br><br>
-              ●通しオンコール（土日祝）<br>
-              9:00～翌朝8:30（勤務時間は応相談）<br>
-              宅待機　4万円+出動手当2万円/往診1回につき<br>
-              クリニック待機　8万円+出動手当1万円/往診1回につき
-            </td>
-          </tr>
-          <tr>
-            <th>待遇</th>
-            <td>理事・院長・副院長など可</td>
-          </tr>
-          <tr>
-            <th>勤務時間</th>
-            <td>
-              9時～17時30分<br>
-              土日祝日・年末年始5日・夏期最大5日・週1～2日研究日あり<br>
-              ※土曜勤務も可
-              </td>
-          </tr>
-          <tr>
-            <th>診療科目</th>
-            <td>
-              内科、外科、呼吸器内科、消化器内科、神経内科、循環器内科、泌尿器科、形成外科、整形外科、リハビリ科、腫瘍内科、緩和ケア科、皮膚科、精神科、訪問診療、在宅医療
-            </td>
-          </tr>
-          <tr>
-            <th>福利厚生・制度</th>
-            <td>
-              60歳（再雇用制度65歳まであり）<br>
-              通勤手当<br>
-              車通勤可<br>
-              昇給あり<br>
-              賞与あり（賞与ありの給与設定も可能です）<br>
-              出産祝い<br>
-              結婚祝い<br>
-              団体医師賠償責任保険加入<br>
-              入職祝い<br>
-              退職金制度あり（勤続5年以上を対象）<br>
-              健康診断<br>
-              予防接種<br>
-              学会参加費用補助<br>
-              開業支援プログラムあり
-            </td>
-          </tr>
-          <tr>
-            <th>設備</th>
-            <td>
-              レントゲン<br>
-              心電図<br>
-              超音波断層装置<br>
-              電子カルテなど
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <a href="/contact/" class="btn fadeIn">
-        <p>応募・お問い合わせはこちら</p>
-      </a>
-    </div>
-  </section>
-
-  <section id="office">
-    <div class="inn office_inn">
-      <h3 class="fadeIn">医療事務、看護師、救命士、社会福祉士</h3>
-      <table class="fadeIn">
-        <tbody>
-          <tr>
-            <th>
-              給与例<br>
-              <span>【常勤／<br class="sp">中途・新卒採用】</span>
-            </th>
-            <td>
-              ●事務部<br>
-              経営企画室・管理職　650,000円～<br>
-              医事課（レセプト）　250,000円～<br>
-              総務課　230,000円～<br>
-              本部総務課（六本木）　230,000円～<br>
-              診療同行事務　230,000円～
-              <br><br>
-              ●連携室<br>
-              看護師　280,000円～<br>
-              社会福祉士　260,000円～<br>
-              介護支援専門員　250,000円～<br>
-              相談員（無資格可）　230,000円～
-              <br><br>
-              ●広報部<br>
-              広報　300,000円～
-              <br><br>
-              ●看護部<br>
-              看護師（訪問）　正 370,000円～／准 350,000円～<br>
-              看護師（外来）　280,000円～<br>
-              救命士　280,000円～<br>
-              診療アシスタント（看護助手）※資格は問いません	　260,000円～
-              <br><br>
-              ●技術部<br>
-              放射線技師　東京 300,000円～
-              <br><br>
-              ●運転部<br>
-              診療ドライバー　埼玉 210,000円～／千葉 210,000円～／東京 240,000円～／東京（夜間専従）　250,000円～
-            </td>
-          </tr>
-          <tr>
-            <th>
-              給与例<br>
-              <span>【非常勤／<br class="sp">中途・新卒採用】</span>
-            </th>
-            <td>
-              ●事務部<br>
-              総務課　時給 1,400円～<br>
-              診療同行事務　時給 1,400円～
-              <br><br>
-              ●連携室<br>
-              正・准 看護師　時給 1,800円～<br>
-              社会福祉士　時給 1,400円～<br>
-              介護支援専門員　時給 1,400円～<br>
-              相談員　時給 1,400円～
-              <br><br>
-              ●看護部<br>
-              看護師（訪問）　正・准 時給 1,800円～<br>
-              看護師（外来）　正・准 時給 1,800円～
-              <br><br>
-              ●運転部<br>
-              診療ドライバー　時給 1,400円～
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <a href="/contact/" class="btn fadeIn">
-        <p>応募・お問い合わせはこちら</p>
-      </a>
-    </div>
-  </section>
-
   <section id="foods">
     <div class="inn foods_inn">
       <h3 class="fadeIn">飲食事業部</h3>
@@ -265,23 +86,40 @@
         <tbody>
           <tr>
             <th>給与例</th>
-            <td>000,000円</td>
+            <td>
+              月給 ￥310,000 〜 ￥410,000<br>
+              ※固定残業代￥100,000〜￥130,000（30時間相当分）を含む。<br>
+              ※30時間を超える残業代は追加で支給する。
+            </td>
           </tr>
           <tr>
             <th>雇用形態</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>正社員</td>
           </tr>
           <tr>
             <th>待遇</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>
+              試用期間３か月<br>
+              通勤手当（社内規定あり）<br>
+              社会保険完備（協会けんぽ 、労災保険、雇用保険、厚生年金保険）<br>
+              昇給あり（年1回あり）<br>
+              賞与あり（年2回あり）最大6ヵ月分支給！<br>
+              年末年始休み<br>
+              有給休暇あり<br>
+              健康診断あり<br>
+            </td>
           </tr>
           <tr>
             <th>勤務時間</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>17:00～ 翌朝 3:00 or 5:00 （シフト制）</td>
           </tr>
           <tr>
             <th>休日休暇</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>
+              月9日休み<br>
+              年末年始休みあり<br>
+              有給休暇あり
+            </td>
           </tr>
         </tbody>
       </table>
@@ -290,7 +128,6 @@
       </a>
     </div>
   </section>
-
   <section id="ip">
     <div class="inn ip_inn">
       <h3 class="fadeIn">IP事業部</h3>
@@ -298,23 +135,37 @@
         <tbody>
           <tr>
             <th>給与例</th>
-            <td>000,000円</td>
+            <td>月給 ￥240,000 〜 ￥410,000</td>
           </tr>
           <tr>
             <th>雇用形態</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>正社員</td>
           </tr>
           <tr>
             <th>待遇</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>
+              試用期間３か月<br>
+              通勤手当（社内規定あり）<br>
+              社会保険完備（協会けんぽ 、労災保険、雇用保険、厚生年金保険）<br>
+              昇給あり（年1回あり）<br>
+              賞与あり（年2回あり）<br>
+              年末年始休み<br>
+              有給休暇あり<br>
+              健康診断あり
+            </td>
           </tr>
           <tr>
             <th>勤務時間</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>10:00～ 19:00（休憩1時間）</td>
           </tr>
           <tr>
             <th>休日休暇</th>
-            <td>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。</td>
+            <td>
+              土日祝日休み<br>
+              年末年始休み<br>
+              夏季休暇あり<br>
+              有給休暇あり
+            </td>
           </tr>
         </tbody>
       </table>
@@ -323,5 +174,4 @@
       </a>
     </div>
   </section>
-
 <?php get_footer(); ?>
