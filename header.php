fe93a0afc482e90b56e3d4f617aa74735ed98b4b
keito noguchi <noguchi@hug-inc.co.jp>
2025-08-19 20:38:38 +0900

    タイトル・ディスクリプション修正

--- header.php
+++ header.php
@@ -2,8 +2,25 @@
 <html>
 <head>
   <meta charset="UTF-8">
+  <?php if ( is_page('service') ) : ?>
+  <title>事業内容｜株式会社Growoods</title>
+  <meta name="description" content="MS事業部（メディカルサービス）医療法人の運営サポート、コンサルティング事業、M&A事業、開業支援、飲食事業部飲食店の運営サポート、スタッフ派遣、コンサルティング事業、IP事業部IPコンテンツを活用した飲食店の販売促進、認知拡大、マーケティング">
+  <?php elseif ( is_page('develop') ) : ?>
+  <title>事務所・店舗｜株式会社Growoods</title>
+  <meta name="description" content=" MS事業部（メディカルサービス）ふたば在宅クリニック六本木本部、錦糸町院、新小岩院、北千住院、埼玉院、北習志野院、佐倉院、八千代院、本八幡院。飲食事業部 dining bar REDD、dining bar REDD2、BAR Symbol">
+  <?php elseif ( is_page('recruit') ) : ?>
+  <title>採用情報｜株式会社Growoods</title>
+  <meta name="description" content="わたしたちは、「医・食から笑顔を」を掲げ、医療機関や飲食店を支えるサポート・コンサルティングをはじめ、IP事業など、多彩な取り組みを展開しています。医療と食の充実こそが、日本の未来を育むと信じ、常に進化を続けています。今、そんな私たちのビジョンに共鳴し、新たな価値を共に生み出す仲間を求めています。">
+  <?php elseif ( is_page('company') ) : ?>
+  <title>会社概要｜株式会社Growoods</title>
+  <meta name="description" content="「医・食から笑顔を」人が生きていく上で欠かせない「医・食」その充実が日本の未来のためになると信じて、進化し続けます。">
+  <?php elseif ( is_page(array('contact','confirm','thanks')) ) : ?>
+  <title>お問い合わせ｜株式会社Growoods</title>
+  <meta name="description" content="事業のご相談・ご質問からリクルートに関する事まで、お気軽にお問い合わせください。2営業日以内でのご返事を原則と致しますが、それ以上の返信が無き場合は稀にシステムトラブル等からご連絡頂けていない場合もございます。その場合はお手数ではありますが再度お電話でのお問い合わせをお願い致します。">
+  <?php else : ?>
   <title><?php bloginfo( 'name' ); ?></title>
-  <meta name="description" content="Growoods">
+  <meta name="description" content="人が生きていく上で欠かせない「医・食」その充実が日本の未来のためになると信じて、常に進化し続けます。社名のGrowoodsには、小さな芽が大きな森に育つように、「Grow-Woods」という願いが込められています。">
+  <?php endif; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
